f8636d8cdfda860662d63ff2112f204b23b3e026
Administrator <1109546392@qq.com>
2018-09-13 16:46:58 +0800

    封装一个返回结果, 状态码

---
+++ src/main/java/com/jack/common/RequestStatus.java
@@ -0,0 +1,13 @@
+package com.jack.common;
+
+public class RequestStatus {
+    // 成功
+    public static int SUCCESS = 200;
+    // 服务器异常
+    public static int ERROR = 500;
+    // 失败
+    public static int FILED = 250;
+    // 处理中
+    public static int COMING = 205;
+
+}
