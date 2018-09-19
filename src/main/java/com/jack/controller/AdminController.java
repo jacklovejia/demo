b8fea73276a1a51bd9f6caa51e70d68529443b4b
Administrator <1109546392@qq.com>
2018-09-19 17:23:46 +0800

    不安全的登录功能已搞定, 后期在扩展权限以及安全登录认证的事吧

--- src/main/java/com/jack/controller/AdminController.java
+++ src/main/java/com/jack/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.jack.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.jack.bean.Admin;
 import com.jack.bean.AdminExample;
 import com.jack.common.RequestStatus;
@@ -7,12 +9,12 @@ import com.jack.common.Result;
 import com.jack.mapper.AdminMapper;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/unsafe")
@@ -24,16 +26,27 @@ public class AdminController {
     private AdminMapper adminMapper;
 
     @RequestMapping("/login")
-    public Result login(String adminName, String password){
+    public Result login(@RequestBody String body){
+//    public Result login(String adminName, String password){
+        JSONObject json = JSON.parseObject(body);
+        String adminName = json.getString("adminName");
+        String password = json.getString("password");
         logger.info("账号:"+adminName+",密码:"+password);
+        if(adminName == null || password == null){
+            return Result.build(RequestStatus.ERROR,"账号密码不能为空");
+        }
         AdminExample adminExample = new AdminExample();
         AdminExample.Criteria criteria = adminExample.createCriteria();
-
+        criteria.andAdminnameEqualTo(adminName);
+        criteria.andPasswordEqualTo(password);
         List<Admin> adminList = adminMapper.selectByExample(adminExample);
         if(adminList.size()< 1){
             return Result.build(RequestStatus.FILED,"账号或密码不正确");
         }
-        return Result.ok();
+        String token = UUID.randomUUID().toString();
+        JSONObject tokenJs = new JSONObject();
+        tokenJs.put("token",token);
+        return Result.ok(tokenJs);
     }
 
 }
